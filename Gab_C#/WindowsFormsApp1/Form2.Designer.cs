6a1c2bf268228901664c58e5a5c6ff645093c0e9
Tohy Ny Aina <109283824+TohyNyAina@users.noreply.github.com>
2023-09-15 07:17:20 +0300

    form

--- Gab_C#/WindowsFormsApp1/Form2.Designer.cs
+++ Gab_C#/WindowsFormsApp1/Form2.Designer.cs
@@ -99,7 +99,7 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(31, 11);
+            this.dataGridView1.Location = new System.Drawing.Point(32, 26);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
